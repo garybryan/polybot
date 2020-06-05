b5cffbde314c76439c64c820dda80b419172e11b
Gary Bryan <garybryan@gmail.com>
2020-06-05 20:41:29 +0000

    Pytest for backends

--- conv-backend-chatterbot/app/main_test.py
+++ conv-backend-chatterbot/app/main_test.py
@@ -4,6 +4,11 @@ from main import app
 
 client = TestClient(app)
 
+# def test_read_main():
+# response = client.get("/")
+# assert response.status_code == 200
+# assert response.json() == {"msg": "Hello World"}
+
 
 def test_post_message():
     sent_text = 'Test text'
--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 docker-compose exec conv-backend-adaptor python -m unittest
 #docker-compose exec conv-backend-chatterbot python -m unittest
-docker-compose exec conv-backend-echo python -m unittest
+docker-compose exec conv-backend-echo pytest
