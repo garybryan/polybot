23140751b32ada6508b4959617ed95e9ee2a3d76
Gary Bryan <garybryan@gmail.com>
2020-09-27 20:13:01 +0100

    Missing main files

---
+++ correction-adaptor/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from correction_adaptor.routers import message
+from correction_adaptor.exception_handlers import http_error_handler, connection_error_handler
+from requests.exceptions import HTTPError
+
+app = FastAPI()
+
+app.add_exception_handler(HTTPError, http_error_handler)
+app.add_exception_handler(ConnectionError, connection_error_handler)
+
+app.include_router(message.router)
---
+++ correction-adaptor/main_test.py
@@ -0,0 +1,17 @@
+from correction_adaptor.exception_handlers import (
+    http_error_handler,
+    connection_error_handler,
+)
+from requests.models import HTTPError
+
+
+def test_main(mocker):
+    # include_router = mocker.patch("main.app.include_router")
+
+    from main import app
+
+    assert app.exception_handlers[HTTPError] == http_error_handler
+    assert app.exception_handlers[ConnectionError] == connection_error_handler
+
+    # include_router.assert_called_once_with()
+    # TODO how to make this work?
