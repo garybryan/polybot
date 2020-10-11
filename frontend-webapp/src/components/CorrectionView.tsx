3e4daec86e054653a408e97e0dda15d27fef65d2
Gary Bryan <garybryan@gmail.com>
2020-10-11 17:37:41 +0100

    Always show all suggestions

--- frontend-webapp/src/components/CorrectionView.tsx
+++ frontend-webapp/src/components/CorrectionView.tsx
@@ -10,14 +10,13 @@ export default function CorrectionView({ correction }: CorrectionViewProps) {
     <div className="CorrectionView">
       <div className="Message">{correction.message}</div>
       <ul className="Suggestions">
-        {correction.suggestions.length > 1 &&
-          correction.suggestions.map((suggestion, index) => (
-            <li key={index}>
-              <em>{suggestion.value}</em>
-              {suggestion.short_description &&
-                ` (${suggestion.short_description})`}
-            </li>
-          ))}
+        {correction.suggestions.map((suggestion, index) => (
+          <li key={index}>
+            <em>{suggestion.value}</em>
+            {suggestion.short_description &&
+              ` (${suggestion.short_description})`}
+          </li>
+        ))}
       </ul>
     </div>
   )
