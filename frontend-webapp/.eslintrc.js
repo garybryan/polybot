409ab9fbb22a513511af57d149e78ccd32ca265f
Gary Bryan <garybryan@gmail.com>
2022-04-03 16:36:59 +0100

    Fix eslint prettier

--- frontend-webapp/.eslintrc.js
+++ frontend-webapp/.eslintrc.js
@@ -1,34 +1,30 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
-  parser:  '@typescript-eslint/parser',
+  parser: '@typescript-eslint/parser',
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended',
   ],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  },
+  plugins: ['react'],
+  rules: {},
   settings: {
     react: {
-      version: 'detect'
-    }
-  }
+      version: 'detect',
+    },
+  },
 }
